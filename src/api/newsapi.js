81e9604ffda3410e2854d487f9c2ee49581599aa
ashketchupppp <computerchris101@gmail.com>
2022-03-07 18:47:30 +0000

    remove linter cba

---
+++ src/api/newsapi.js
@@ -0,0 +1,14 @@
+const newsapi_apikey = process.env.NEWSAPI_APIKEY
+
+const newsApiRoot = 'https://newsapi.org'
+const routes = {
+    topHeadlines: ''
+}
+
+function topHeadlines () {
+    fetch()
+}
+
+module.exports = {
+    topHeadlines
+}
